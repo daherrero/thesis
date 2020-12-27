eff821060f98f8b06d25c0138f3f357d3d94f6a9
David Herrero <31295226+daherrero@users.noreply.github.com>
2020-12-27 13:04:41 +0000

    changed query response

--- algorithm_1.py
+++ algorithm_1.py
@@ -1,8 +1,9 @@
 import numpy as np
 import helpers
 
+
 def R_i_Gauss(A, i, epsilon, delta):
-    a_v_i = A[i, ]
+    a_v_i = np.matmul(A, i)
     r = np.power(helpers.r(A), 2)
     sigma_sq = 2 * r * np.log(2/delta) / np.power(epsilon, 2)
     z_i = np.random.normal(0, np.multiply(sigma_sq, np.identity(A.shape[0])))
