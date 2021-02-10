78fcaa3b66de118ee72a00939f01e4cf6165833b
David Herrero <31295226+daherrero@users.noreply.github.com>
2021-02-10 14:49:05 +0000

    included l1_projection code

--- algorithms/helpers.py
+++ algorithms/helpers.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 
 def r(query_matrix):
@@ -7,3 +8,40 @@ def r(query_matrix):
 
 def J(users_inputs):
     return np.amax(users_inputs) - np.amin(users_inputs)
+
+
+# taken from https://wet-robots.ghost.io/simple-method-for-distance-to-ellipse/
+
+
+def ellipse_distance(semi_major, semi_minor, p):
+    px = abs(p[0])
+    py = abs(p[1])
+
+    t = math.pi / 4
+
+    a = semi_major
+    b = semi_minor
+
+    for x in range(0, 3):
+        x = a * math.cos(t)
+        y = b * math.sin(t)
+
+        ex = (a*a - b*b) * math.cos(t)**3 / a
+        ey = (b*b - a*a) * math.sin(t)**3 / b
+
+        rx = x - ex
+        ry = y - ey
+
+        qx = px - ex
+        qy = py - ey
+
+        r = math.hypot(ry, rx)
+        q = math.hypot(qy, qx)
+
+        delta_c = r * math.asin((rx*qy - ry*qx)/(r*q))
+        delta_t = delta_c / math.sqrt(a*a + b*b - x*x - y*y)
+
+        t += delta_t
+        t = min(math.pi/2, max(0, t))
+
+    return (math.copysign(x, p[0]), math.copysign(y, p[1]))
