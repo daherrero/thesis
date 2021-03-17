236cf5477b1e0d4f41120cb06f879cf4aa60e588
David Herrero <31295226+daherrero@users.noreply.github.com>
2021-03-17 20:16:55 +0000

    Minor fix

--- src/algorithm_6.py
+++ src/algorithm_6.py
@@ -11,19 +11,12 @@ def Prot_AdSamp(D, epsilon, queries):
     c_epsilon_r = c_epsilon*r
     response_vector = np.zeros(queries.shape[0])
     for k in range(queries.shape[0]):
-        # print(k)
         users_in_k = np.where(partition == k)[0]
-        # print(users_in_k)
         if len(users_in_k) == 0:
             continue
         y_tilda_sum = 0
         for user in users_in_k:
             q_k_v_i = np.matmul(queries[k, ], D[user, ])
-            # print("qkvi"+str(q_k_v_i))
-            # print("prob1 "+str((1/2)*(1+(q_k_v_i/(c_epsilon*r)))))
-            # print("prob2 "+str((1/2)*(1-(q_k_v_i/(c_epsilon*r)))))
-            # print((1/2)*(1+(q_k_v_i/(c_epsilon*r)))+(1/2)*(1-(q_k_v_i/(c_epsilon*r))))
-            # print("random"+str(np.random.rand()))
             if np.random.rand() < (1/2)*(1+(q_k_v_i/(c_epsilon*r))):
                 y_tilda_sum += c_epsilon_r
             else:
